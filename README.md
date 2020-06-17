fe612987968d745b402e2d9ba81487a4928c2bfa
UndeadBigUnicorn <kravchel16@gmail.com>
2020-06-17 22:30:07 +0300

    add parsing args to enter category and file to check from the terminal; rename practice.py -> main.py

---
+++ README.md
@@ -0,0 +1,3 @@
+## Automatic assessment using NLP
+
+Example usage: `python3 main.py --category analytic hierarchy process' --file text.txt`
--- main.py
+++ main.py
@@ -2,11 +2,24 @@ import scrapper
 from nlp import NLP
 from db import DB
 import log
+import argparse
+
+# Instantiate the parser
+parser = argparse.ArgumentParser(description="Automatic assessment for student works")
+
+# Required positional argument
+parser.add_argument("--category",
+                    help="Required category name. Provide value in ''")
+
+parser.add_argument("--file",
+                    help="Filename of student work to check on")
+
+args = parser.parse_args()
 
 # workflow:
 # 1. get all wikipedia articles that contains given category
 # 2. use nlp in every article, find head words in sentences and highlight them
-# 3. use nlp on wikipedia articles in depth 3 that are in "== See alse ==" section on the original articles
+# 3. use nlp on wikipedia articles in depth 3 that are in "== See also ==" section on the original articles
 # and make the same
 # 4. save data in the db, rocksdb,  etc.
 # 5. train entity recognition models on the saved datasets
@@ -14,7 +27,7 @@ import log
 # 7. check works for plagiarism (wikipedia texts and works between each other)
 
 # category to search
-category = "analytic hierarchy process"
+category = args.category
 
 # init db
 db = DB(category)
@@ -24,7 +37,7 @@ pages = db.get_pages()
 if pages is None:
     pages = scrapper.look_for_articles(db, category)
 
-log.info("Articles: " + "; ".join(db.get_articles()))
+log.debug("Articles: " + "; ".join(db.get_articles()))
 log.info(f"Total Pages: {len(db.get_pages())}")
 
 training_data = db.get_training_data()
@@ -45,6 +58,6 @@ if len(training_data) == 0:
     nlp.train_model()
     log.info("New model was trained")
 
-doc = nlp.read_text("AHP was created by Thomas Saaty to solve matrix by evaluation the hierarchy and establishing priorities")
-
-print([(ent.text, ent.label_) for ent in doc.ents])
+with open(args.file) as f:
+    doc = nlp.read_text(f.read())
+    print([(ent.text, ent.label_) for ent in doc.ents])
